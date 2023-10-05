89b883cd95c547f68d11638117d4a9c3e0fc19fe
Thayna Tolentino <tatarlt7@gmail.com>
2023-10-05 15:31:09 -0300

    .

---
+++ reunioes-acionistas/src/Main.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws ParseException {
+    // Lê os dados de Entrada conforme descrito neste Desafio.
+    Scanner scanner = new Scanner(System.in);
+    String dataInicial = scanner.nextLine();
+    String dataFinal = scanner.nextLine();
+
+    SistemaAcionistas sistemaAcionistas = new SistemaAcionistas();
+    List < String > analises = sistemaAcionistas.obterAnalisesDesempenho(dataInicial, dataFinal);
+
+    for (String analise: analises) {
+      System.out.println(analise);
+    }
+  }
+}
+
+class SistemaAcionistas {
+  public List < String > obterAnalisesDesempenho(String dataInicialStr, String dataFinalStr) throws ParseException {
+    SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Date dataInicial = df.parse(dataInicialStr);
+    Date dataFinal = df.parse(dataFinalStr);
+
+    // Simula uma base da dados em uma lista de análises:
+    List < Analise > analises = new ArrayList < > ();
+    analises.add(new Analise(df.parse("01/01/2023"), "Analise de Desempenho Financeiro"));
+    analises.add(new Analise(df.parse("15/02/2023"), "Analise de Riscos e Exposicoes"));
+    analises.add(new Analise(df.parse("31/03/2023"), "Analises Corporativas"));
+    analises.add(new Analise(df.parse("01/04/2023"), "Analise de Politicas e Regulamentacoes"));
+    analises.add(new Analise(df.parse("15/05/2023"), "Analise de Ativos"));
+    analises.add(new Analise(df.parse("30/06/2023"), "Analise de Inovacao e Tecnologia"));
+
+    // Implementa o filtro das análises dentro do período especificado:
+    List < String > analisesFiltradas = new ArrayList < > ();
+    for (Analise analise: analises) {
+      // Alteração: verifica se a data da análise é igual ou maior que a data inicial e igual ou menor que a data final.
+      if (analise.data.compareTo(dataInicial) >= 0 && analise.data.compareTo(dataFinal) <= 0) {
+        analisesFiltradas.add(analise.descricao);
+      }
+    }
+
+    // Adiciona a análise "Analise de Politicas e Regulamentacoes" à lista de análises filtradas:
+    // (opcional)
+    //analisesFiltradas.add("Analise de Politicas e Regulamentacoes");
+
+    // Retorna a lista de análises filtradas:
+    return analisesFiltradas;
+  }
+
+  class Analise {
+    Date data;
+    String descricao;
+
+    public Analise(Date data, String descricao) {
+      this.data = data;
+      this.descricao = descricao;
+    }
+  }
+}
