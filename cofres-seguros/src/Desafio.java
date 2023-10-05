3288803c964bbeb3591078a78088dc77571a97b3
Thayna Tolentino <tatarlt7@gmail.com>
2023-10-05 19:56:00 -0300

    .

--- cofres-seguros/src/Desafio.java
+++ cofres-seguros/src/Desafio.java
@@ -25,7 +25,7 @@ class CofreDigital extends Cofre {
     }
 
     public boolean validarSenha(int confirmacaoSenha) {
-        return confirmacaoSenha == this.senha;
+        return this.senha == confirmacaoSenha;
     }
 }
 
@@ -38,32 +38,35 @@ class CofreFisico extends Cofre {
 
 public class Desafio {
     public static void main(String[] args) {
-        // Lê o tipo de cofre (primeira linha da entrada)
-        Scanner scanner = new Scanner(System.in);
-        String tipoCofre = scanner.nextLine();
+        try (// Lê o tipo de cofre (primeira linha da entrada)
+        Scanner scanner = new Scanner(System.in)) {
+            String tipoCofre = scanner.nextLine();
 
-        // Implementa a condição necessária para a verificação dos cofres seguros:
-        if (tipoCofre.equalsIgnoreCase("digital")) {
-            // Se for um Cofre Digital, leia a senha e a confirmação de senha
-            int senha = scanner.nextInt();
-            int confirmacaoSenha = scanner.nextInt();
+            // Implementa a condição necessária para a verificação dos cofres seguros:
+            if (tipoCofre.equalsIgnoreCase("digital")) {
+                // Se for um Cofre Digital, leia a senha e a confirmação de senha
+                int senha = scanner.nextInt();
+                int confirmacaoSenha = scanner.nextInt();
 
-            // Crie um Cofre Digital
-            CofreDigital cofreDigital = new CofreDigital(senha);
+                // Crie um Cofre Digital
+                CofreDigital cofreDigital = new CofreDigital(senha);
 
-            // Verifique se a senha está correta e imprima as informações do cofre
-            if (cofreDigital.validarSenha(confirmacaoSenha)) {
-                System.out.println("Tipo: " + cofreDigital.tipo);
-                System.out.println("Metodo de abertura: " + cofreDigital.metodoAbertura);
-                System.out.println("Cofre aberto!");
-            } else {
-                System.out.println("Senha incorreta!");
+                // Verifique se a senha está correta e imprima as informações do cofre
+                if (cofreDigital.validarSenha(confirmacaoSenha)) {
+                    System.out.println("Tipo: " + cofreDigital.tipo);
+                    System.out.println("Metodo de abertura: " + cofreDigital.metodoAbertura);
+                    System.out.println("Cofre aberto!");
+                } else {
+                    System.out.println("Tipo: " + cofreDigital.tipo);
+                    System.out.println("Metodo de abertura: " + cofreDigital.metodoAbertura);
+                    System.out.println("Senha incorreta!");
+                }
+            } else if (tipoCofre.equalsIgnoreCase("fisico")) {
+                // Se for um Cofre Físico, crie um Cofre Físico e imprima suas informações
+                CofreFisico cofreFisico = new CofreFisico();
+                System.out.println("Tipo: " + cofreFisico.tipo);
+                System.out.println("Metodo de abertura: " + cofreFisico.metodoAbertura);
             }
-        } else if (tipoCofre.equalsIgnoreCase("fisico")) {
-            // Se for um Cofre Físico, crie um Cofre Físico e imprima suas informações
-            CofreFisico cofreFisico = new CofreFisico();
-            System.out.println("Tipo: " + cofreFisico.tipo);
-            System.out.println("Metodo de abertura: " + cofreFisico.metodoAbertura);
         }
     }
-}.
+}
